95b878f5bef378811c3dc33ba887da4965ab9f76
Matol66 <mateusz.lewandowski@ddspoland.com>
2014-11-25 15:59:04 +0100

    added comments

--- svg.crossbrowser.js
+++ svg.crossbrowser.js
@@ -1,16 +1,13 @@
-	/******************** SVG CrossBrowser FIX by m66design */
-$( document ).ready(function() {	
-	$('svg').each(function(){
-		var viewbox = this.getAttribute('viewBox');
-		var elwidth = $(this).width();
-		var viewboxArr = viewbox.split(" ");
-		var elNewHeight = (elwidth * viewboxArr[3] / viewboxArr[2]);
-		$(this).attr({
-			'width' : elwidth,
-			'height' : elNewHeight
-		}).css({
-			'width' : elwidth,
-			'height' : elNewHeight
-		});
-	});
+$('svg').each(function(){
+    var viewbox = this.getAttribute('viewBox');
+    var elwidth = $(this).width();  // SIZE IS CALCULATED BY ELEMENT WIDTH FORM CSS
+    var viewboxArr = viewbox.split(" ");
+    var elNewHeight = (elwidth * viewboxArr[3] / viewboxArr[2]);  //HOLD RATIO
+    $(this).attr({ // ADD ATTRIBUTES FOR SAFARI AND IE
+        'width' : elwidth,
+        'height' : elNewHeight
+    }).css({    // CSS IS GOOD FOR OTHER BROWSERS
+        'width' : elwidth,
+        'height' : elNewHeight
+    });
 });
